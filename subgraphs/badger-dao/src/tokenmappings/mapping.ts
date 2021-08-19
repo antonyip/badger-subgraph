5e1920553f5903cc5a42c94272a6a5f9a930f065
Anton Yip <Anton_Yip@hotmail.com>
2021-08-19 04:19:56 -0700

    update to tricrypto2 version

--- subgraphs/badger-dao/src/tokenmappings/mapping.ts
+++ subgraphs/badger-dao/src/tokenmappings/mapping.ts
@@ -1,8 +1,5 @@
 export { handleShareTransfer, handleAffiliateShareTransfer } from './yVault';
 export { handleHarvest } from './strategy';
-//export { handleStaked, handleUnstaked } from './geyser';
-//export { handleFarmHarvest } from './farm';
-//export { handleSushiHarvest } from './sushi';
 export { handleBadgerTransfer } from './badger';
 export { handleDiggTransfer } from './digg';
 export { handleIbBTCTransfer} from './ibbtc'

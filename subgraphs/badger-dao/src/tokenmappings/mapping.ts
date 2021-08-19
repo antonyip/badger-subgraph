1ad0c84b48c52222b36aad9a2418805951209d0d
Anton Yip <Anton_Yip@hotmail.com>
2021-08-19 04:28:29 -0700

    partial commit for treedistribution

--- subgraphs/badger-dao/src/tokenmappings/mapping.ts
+++ subgraphs/badger-dao/src/tokenmappings/mapping.ts
@@ -3,3 +3,4 @@ export { handleHarvest } from './strategy';
 export { handleBadgerTransfer } from './badger';
 export { handleDiggTransfer } from './digg';
 export { handleIbBTCTransfer} from './ibbtc'
+export { handleTreeDistribution } from './mappings/treeDistribution';

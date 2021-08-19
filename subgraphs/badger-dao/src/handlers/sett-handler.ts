c87fe9a8ef27c91ae84fa2c8b6c4c3f962b25f26
Anton Yip <Anton_Yip@hotmail.com>
2021-08-19 08:11:54 -0700

    add back missing sett call

--- subgraphs/badger-dao/src/handlers/sett-handler.ts
+++ subgraphs/badger-dao/src/handlers/sett-handler.ts
@@ -1,6 +1,6 @@
 import { Address, BigInt } from '@graphprotocol/graph-ts';
 import { Sett } from '../../generated/schema';
-import { Transfer } from '../../generated/templates/SettVault/BadgerSett';
+import { Transfer } from '../../generated/BADGER/V1Contract';
 import { NO_ADDR, SettType } from '../constants';
 import { loadAffiliateSett } from '../entities/affiliate-sett';
 import { loadSett } from '../entities/badger-sett';
@@ -8,6 +8,7 @@ import { loadSettV2 } from '../entities/badger-sett-v2';
 import { loadSettSnapshot } from '../entities/sett-snapshot';
 import { isValidUser, loadUser } from '../entities/user';
 import { depositBalance, loadUserBalance, withdrawBalance } from '../entities/user-sett-balance';
+import { handleShareTransfer } from '../tokenmappings/yVault';
 
 export function handleTransfer(event: Transfer): void {
   let timestamp = event.block.timestamp.toI32();
@@ -15,6 +16,7 @@ export function handleTransfer(event: Transfer): void {
   let to = event.params.to;
   let value = event.params.value;
   handleSettTokenTransfer(timestamp, event.address, SettType.v1, from, to, value);
+  handleShareTransfer(event);
 }
 
 export function depositSett(timestamp: i32, sett: Sett, share: BigInt, token: BigInt): void {

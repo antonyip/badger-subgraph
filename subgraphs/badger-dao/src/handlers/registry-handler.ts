7de42a6dcc45e5067d0560cbe0595fa316b6a335
l0c4t0r <l0c4t0r@gmail.com>
2021-08-26 23:02:19 +0800

    Track status of setts in registry

--- subgraphs/badger-dao/src/handlers/registry-handler.ts
+++ subgraphs/badger-dao/src/handlers/registry-handler.ts
@@ -1,13 +1,8 @@
 import { Sett } from '../../generated/schema';
-import { SettVault, SettVaultV2 } from '../../generated/templates';
+import { SettVault } from '../../generated/templates';
 import { NewVault, PromoteVault, RemoveVault } from '../../generated/VaultRegistry/VaultRegistry';
-import {
-  NewVault as NewVaultV2,
-  PromoteVault as PromoteVaultV2,
-  RemoveVault as RemoveVaultV2
-} from '../../generated/VaultRegistryV2/VaultRegistryV2';
-import { loadSett } from '../entities/badger-sett';
-import { loadSettV2 } from '../entities/badger-sett-v2';
+import { loadSett, updateSettStatus } from '../entities/badger-sett';
+
 
 // TODO: consider how to differentiate on author
 export function handleNewVault(event: NewVault): void {
@@ -19,37 +14,10 @@ export function handleNewVault(event: NewVault): void {
   }
 }
 
-// TODO: potentially use for upgrading vault state vs. registering new vaults
-// eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars-experimental
-export function handlePromoteVault(event: PromoteVault): void {}
-
-// TODO: consider vault state (active, deprecated, guarded) via new / promote
-// eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars-experimental
-export function handleRemoveVault(event: RemoveVault): void {}
-
-export function handleNewVaultV2(event: NewVaultV2): void {
-  let vault = event.params.vault;
-  let version = event.params.version;
-
-  if (version == "v1") {
-    let sett = loadSett(vault);
-    if (sett == null) {
-      SettVault.create(vault);
-      loadSett(vault).save();
-    }
-  } else if (version == "v2") {
-    let settV2 = loadSettV2(vault);
-    if (settV2 == null) {
-      SettVaultV2.create(vault);
-      loadSettV2(vault).save();
-    }
-  }
+export function handlePromoteVault(event: PromoteVault): void {
+  updateSettStatus(event.params.vault, "", "open");
 }
 
-// TODO: potentially use for upgrading vault state vs. registering new vaults
-// eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars-experimental
-export function handlePromoteVaultV2(event: PromoteVaultV2): void {}
-
-// TODO: consider vault state (active, deprecated, guarded) via new / promote
-// eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars-experimental
-export function handleRemoveVaultV2(event: RemoveVaultV2): void {}
+export function handleRemoveVault(event: RemoveVault): void {
+  updateSettStatus(event.params.vault, "", "unregistered");
+}
---
+++ subgraphs/badger-dao/src/handlers/registry-v2-handler.ts
@@ -0,0 +1,37 @@
+import { Sett } from '../../generated/schema';
+import { SettVault, SettVaultV2 } from '../../generated/templates';
+import { NewVault, PromoteVault, DemoteVault, RemoveVault } from '../../generated/VaultRegistryV2/VaultRegistryV2';
+import { loadSett, updateSettStatus, SettStatusString } from '../entities/badger-sett';
+import { loadSettV2 } from '../entities/badger-sett-v2';
+
+
+export function handleNewVault(event: NewVault): void {
+  let vault = event.params.vault;
+  let version = event.params.version;
+
+  let sett = Sett.load(vault.toHexString());
+
+  if (sett == null) {
+    if (version == 'v1') {
+      SettVault.create(vault);
+      loadSett(vault).save();
+    } else if (version == 'v2') {
+      SettVaultV2.create(vault);
+      loadSettV2(vault).save();
+    }
+  }
+}
+
+export function handlePromoteVault(event: PromoteVault): void {  
+  let status = SettStatusString(event.params.status);
+  updateSettStatus(event.params.vault, event.params.version, status);
+}
+
+export function handleDemoteVault(event: DemoteVault): void {
+  let status = SettStatusString(event.params.status);
+  updateSettStatus(event.params.vault, event.params.version, status);
+}
+
+export function handleRemoveVault(event: RemoveVault): void {
+  updateSettStatus(event.params.vault, "", "unregistered");
+}
